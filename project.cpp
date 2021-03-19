8139a0bafc9b4a0c1a261316bbd6223f0dbbf849
Tim-eyes <18228057503@163.com>
2021-03-19 14:36:51 +0800

    set times and waitpid

--- project.cpp
+++ project.cpp
@@ -1,22 +1,37 @@
 #include "project.h"
 #include <iostream>
 #include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/times.h>
+#include <sys/types.h>
 #include <iomanip>
 #include <cstring>
 using namespace Project;
 using namespace std;
+
+double Monitor::getTick(clock_t time){
+    long tps=sysconf(_SC_CLK_TCK);
+    return (double)time/tps;
+}
+
 void Monitor::set_pid(int pid){
     this->process_pid=pid;
 }
-void Monitor::set_systermTime(double time){
-    this->system_time=time;
+void Monitor::set_systermTime(clock_t time){
+    this->system_time=getTick(time);
 }
-void Monitor::set_userTime(double time){
-    this->user_time=time;
+void Monitor::set_userTime(clock_t time){
+    this->user_time=getTick(time);
 }
-void Monitor::set_timeElapsed(double time){
-    this->time_elapsed=time;
+void Monitor::set_timeElapsed(clock_t time){
+    this->time_elapsed=getTick(time);
 }
+
+void Monitor::set_all(int pid,clock_t use_time,clock_t system_time,clock_t time_elasped){
+
+}
+
 int Monitor::get_pid() const{
     return this->process_pid;
 }
@@ -31,12 +46,33 @@ double Monitor::get_timeElapsed() const{
 }
 void Monitor::execute_command(char *command[]){
     int process;
+    int status;
+    start=times(&t_start);
     process=fork();
-    if(process==0){
-        execvp("ls",command);
+    if(process<0){
+        status=-1;
+    }
+    else if(process==0){    
+        execvp(command[0],command);
         cout<<"fail";
     }
+    else{
+        while(waitpid(process,&status,0)<0){
+            if(errno!=EINTR){
+                status=-1;
+                break;
+            }
+        }
+    }
+    end=times(&t_end);
     set_pid(process);
+    set_timeElapsed(end-start);
+    set_userTime(t_end.tms_cutime);
+    set_systermTime(t_end.tms_cstime);
+
+    //kill process
+    //kill(getpid(),SIGTERM);
+    //kill(process,SIGTERM);
 }
 namespace Project{
     ostream& operator<<(ostream& out,const Monitor& m){
--- project.h
+++ project.h
@@ -1,8 +1,10 @@
 #ifndef PROJECT_H
 #define PROJECT_H
-#include<iostream>
-#include<vector>
-#include<string>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <time.h>
+#include <sys/times.h>
 
 namespace Project{
     class Monitor
@@ -10,18 +12,31 @@ namespace Project{
     private:
         char **command;
         int process_pid=0;
+        clock_t start,end;
+        // /*
+        // struct tms{
+        //     clock_t tms_utime; /* user cpu time */
+        //     clock_t tms_stime; /* system cpu time */
+        //     clock_t tms_cutime; /* user cpu time of children */
+        //     clock_t tms_cstime; /* system cpu time of children */
+        // }
+        // */
+        struct tms t_start,t_end; 
         double user_time=0;
         double system_time=0;
         double time_elapsed=0;
+
+        double getTick(clock_t time);
+
     public:
         Monitor(char *command[]):command(command){
             execute_command(command);
         };
         void set_pid(int pid);
-        void set_userTime(double time);
-        void set_systermTime(double time);
-        void set_timeElapsed(double time);
-        void set_all(double pid,double use_time,double system_time,double time_elasped);
+        void set_userTime(clock_t time);
+        void set_systermTime(clock_t time);
+        void set_timeElapsed(clock_t time);
+        void set_all(int pid,clock_t use_time,clock_t system_time,clock_t time_elasped);
         void execute_command(char *command[]);
         int get_pid() const;
         double get_userTime() const;
